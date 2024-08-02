72eb114385f42921fb3bb24f3dfbaed494aaea40
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-08-02 18:02:45 +0900

    JpaAuditing  어노테이션 추가

--- src/main/java/com/example/cep/CepApplication.java
+++ src/main/java/com/example/cep/CepApplication.java
@@ -2,8 +2,10 @@ package com.example.cep;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class CepApplication {
 
   public static void main(String[] args) {
