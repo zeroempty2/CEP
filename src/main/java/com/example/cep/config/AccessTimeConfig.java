de9d1fa7676c474bc8971efea64433858d9b5f01
2zeroempty@gmail.com <2zeroempty@gmail.com>
2024-09-24 19:08:48 +0900

    TimeBaseFillter 생성자 변경

--- src/main/java/com/example/cep/config/AccessTimeConfig.java
+++ src/main/java/com/example/cep/config/AccessTimeConfig.java
@@ -1,7 +1,10 @@
 package com.example.cep.config;
 
+import com.example.cep.security.UserDetailsServiceImpl;
+import com.example.cep.util.JwtUtil;
 import com.example.cep.util.scheduler.AccessControl;
 import com.example.cep.util.scheduler.TimeBasedFilter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,12 +12,15 @@ import org.springframework.core.Ordered;
 
 //커스텀 필터 적용
 @Configuration
+@RequiredArgsConstructor
 public class AccessTimeConfig {
+  private final JwtUtil jwtUtil;
+  private final UserDetailsServiceImpl userDetailsService;
   @Bean
   public FilterRegistrationBean<TimeBasedFilter> timeBasedFilterRegistration(
       AccessControl accessControl) {
     FilterRegistrationBean<TimeBasedFilter> registrationBean = new FilterRegistrationBean<>();
-    registrationBean.setFilter(new TimeBasedFilter(accessControl));
+    registrationBean.setFilter(new TimeBasedFilter(accessControl,userDetailsService,jwtUtil));
     registrationBean.setOrder(Ordered.LOWEST_PRECEDENCE);
     registrationBean.addUrlPatterns("/*");
     return registrationBean;
